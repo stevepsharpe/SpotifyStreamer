d687b2c8f134f684888f764f99ebbb01a36ad3a0
Steve P. Sharpe <stevepsharpe@me.com>
2015-06-16 09:40:57 +0100

    add crashlytics

--- app/src/main/java/com/stevepsharpe/spotifystreamer/ui/activities/SearchActivity.java
+++ app/src/main/java/com/stevepsharpe/spotifystreamer/ui/activities/SearchActivity.java
@@ -2,10 +2,11 @@ package com.stevepsharpe.spotifystreamer.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
+import com.crashlytics.android.Crashlytics;
 import com.stevepsharpe.spotifystreamer.R;
+
+import io.fabric.sdk.android.Fabric;
 
 
 public class SearchActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
